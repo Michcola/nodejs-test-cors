2b1eb318650405123ae424d2847b68c8d15e7570
mich <843298603@qq.com>
2019-04-07 21:31:53 +0800

    第二次增加了promise

--- main.js
+++ main.js
@@ -1,51 +1,54 @@
+    
 window.jQuery = function(nodeOrSelector){
     let nodes = {}
     nodes.addClass = function(){}
     nodes.html = function(){}
     return nodes
-}
-window.$ = window.jQuery
-
-window.jQuery.ajax = function({url,method,body,successFn,failFn,headers}){
-    let request = new XMLHttpRequest()
-    request.open(method,url)   //配置request
-    for (let key in headers) {
+  }
+  window.$ = window.jQuery
+  
+  window.Promise = function(fn){
+    // ...
+    return {
+      then: function(){}
+    }
+  }
+  
+  window.jQuery.ajax = function({url, method, body, headers}){
+    return new Promise(function(resolve, reject){
+      let request = new XMLHttpRequest()
+      request.open(method, url) // 配置request
+      for(let key in headers) {
         let value = headers[key]
-        request.setRequestHeader(key,value)
-    }
-    request.onreadystatechange = () =>{
-        if (request.readyState===4) {
-            if (request.status >= 200 && request.status<300) {
-                successFn.call(undefined, request.responseText)
-            }else if(request.status >= 400){
-                failFn.call(undefined,request)
-            }
+        request.setRequestHeader(key, value)
+      }
+      request.onreadystatechange = ()=>{
+        if(request.readyState === 4){
+          if(request.status >= 200 && request.status < 300){
+            resolve.call(undefined, request.responseText)
+          }else if(request.status >= 400){
+            reject.call(undefined, request)
+          }
         }
-    }
-    request.send(body)
-}
-
-function f1(responseText){}
-function f2(responseText){}
-
-myButton.addEventListener('click', (e) =>{
-    window.jQuery.ajax({
-        url: './ffff',
-        method: 'post',
-        headers: {
-            'content-type':'application/x-www-form-urlencoded',
-            'frank':'18'
-        },
-        body: 'a=1&b=2',
-        successFn: (x)=>{
-            f1.call(undefined,x)
-            f2.call(undefined,x)
-        },
-        failFn: (x)=>{
-             console.log(x)
-             console.log(x.status)
-             console.log(x.responseText)
-            }
+      }
+      request.send(body)
     })
-})
-
+  }
+  
+  myButton.addEventListener('click', (e)=>{
+    let promise = window.jQuery.ajax({
+      url: '/xxx',
+      method: 'get',
+      headers: {
+        'content-type':'application/x-www-form-urlencoded',
+        'frank': '18'
+      }
+    })
+  
+    promise.then(
+      (text)=>{console.log(text)},
+      (request)=>{console.log(request)}
+    )
+  
+  })
+  
